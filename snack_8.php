81dfaadc8cdfa437fd178832f99e8ad1a51f0122
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-02 17:19:43 +0100

    1.1th working on 8th snack

--- snack_8.php
+++ snack_8.php
@@ -5,15 +5,25 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
 */
 
 //checking that name is set and its length is longer than 3
-var_dump(isset($_GET['name']) && strlen($_GET['name']) > 3);
+var_dump('name: ', isset($_GET['name']) && strlen($_GET['name']) > 3);
 
 //checking if mail is set and it contains the @ value
-var_dump(isset($_GET['mail']) && str_contains($_GET['mail'], '@'));
+var_dump('mail:', isset($_GET['mail']) && str_contains($_GET['mail'], '@'));
 
 //checking if is contained and if it's an integer
-var_dump(isset($_GET['age']) && is_numeric($_GET['age']));
+var_dump('age: ', isset($_GET['age']) && is_numeric($_GET['age']));
 
 $canAccess;
+
+if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+    var_dump('name, mail and age are set');
+    if (strlen($_GET['name']) > 3 && str_contains($_GET['mail'], '@') && is_numeric($_GET['age'])) {
+        var_dump('every value matches requirements, you can access your personal area');
+        $canAccess = true;
+    }
+} else {
+    var_dump('error');
+}
 
 ?>
 
@@ -29,18 +39,60 @@ $canAccess;
 
 <body>
 
-    <form action="get" class="d-flex justify-content-center mt-5">
-        <label for="name" class="me-1">name</label>
-        <input type="text" class="me-2 text-center" name="name" id="name" required>
 
-        <label for="mail" class="me-1">mail</label>
-        <input type="text" class="me-2 text-center" name="mail" id="mail" required>
+    <div class="container">
+        <p class="col-4 text-center text-primary my-2"><?php echo $canAccess ? 'Welcome!' : 'Access denied' ?></p>
+        <form action="" method="get">
+            <div class="wrapper">
+                <label for="name">Name</label>
+                <input class="form-control" type="text" placeholder="write here your name" name="name" id="input_name" required>
+            </div>
+            <div class="wrapper">
+                <label for="mail">Mail</label>
+                <input class="form-control" type="text" placeholder="write here your mail" name="mail" id="input_mail">
+            </div>
+            <div class="wrapper">
+                <label for="age">Age</label>
+                <input class="form-control" type="text" placeholder="write here your age" name="age" id="input_age">
+            </div>
+            <button class="btn btn-primary" type="submit">submit</button>
+        </form>
+    </div>
 
-        <label for="age" class="me-1">age</label>
-        <input type="text" class="me-2 text-center" name="age" id="age" required>
+    <style>
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
 
-        <button class="bg-primary">submit</button>
-    </form>
+        .wrapper {
+            display: flex;
+            margin-top: 1rem;
+
+            >label {
+                margin-right: .5rem;
+                margin-top: .1rem;
+            }
+        }
+
+        form {
+            margin-top: .5rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        button {
+            margin-top: 1rem;
+        }
+
+        button:hover {
+            background-color: red;
+        }
+    </style>
+
 </body>
 
 </html>
